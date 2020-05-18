7241a2abd49d7ee1a7f070d02ffbb4a8adce6e93
Aleksa <lukicaleksa04@gmail.com>
2020-05-18 11:15:33 +0200

    Kompletirana tuning stranica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TuningComponent } from './tuning/tuning.component';
 
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'tuning', component: TuningComponent }];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
