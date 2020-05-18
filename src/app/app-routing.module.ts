1154a6fcc097b6ad2d758281b9300e3b2eec669e
Aleksa <lukicaleksa04@gmail.com>
2020-05-18 11:38:40 +0200

    LoginComponent html/css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginCmsComponent } from './login-cms/login-cms.component';
 
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent },{path:'login',component:LoginCmsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
